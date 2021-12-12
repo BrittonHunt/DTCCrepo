3d422e17fdfc0d435f77c95ad075d0160c7a658c
Britton Hunt <bhunt6@dtcc.edu>
2021-12-12 13:32:50 -0500

    Check it out!

--- App.js
+++ App.js
@@ -1,49 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, Image, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, Image, View } from "react-native";
 
-// Determines what picture the song uses.
-var songImage='./images/Michael_Jackson_-_Bad.png';
-var songTitle = "Bad";
+// Determine what variables the song uses.
+var songImage = "./images/Michael_Jackson_-_Bad.png";
+var songTitle = "Librarian Girl";
 var artist = "Michael Jackson";
+var songLength = 12780;
 
-export default function App() {
-
+// Create the progress bar
+const Progress = ({ start, length, height }) => {
   return (
-    <View style={styles.body2} >
+    <View
+      style={{
+        height,
+        backgroundColor: "#fff",
+        borderRadius: height,
+        overflow: "hidden",
+      }}
+    >
+      <View
+        style={{
+          height,
+          width: "100%",
+          backgroundColor: "fff",
+          borderRadius: height,
+          position: "absolute",
+          left: 0,
+          top: 0,
+        }}
+      />
+    </View>
+  );
+};
+
+// Main function
+export default function App() {
+  return (
+    <View style={styles.body2}>
       <Image source={require(songImage)} style={styles.img} />
-      <Text>{'\n'}</Text>
-      <Text style = {styles.headerText}>
-        {songTitle} 
-        </Text>
-      <Text style = {styles.bodyText}>
-      {artist}</Text>
-      <StatusBar style="auto"/>
+      <Text>{"\n"}</Text>
+      <Text style={styles.headerText}>{songTitle}</Text>
+      <Text style={styles.bodyText}>
+        {artist}
+        {"\n"}
+      </Text>
+      <StatusBar style="auto" />
+      <Progress start={0} length={songLength} height={20} />
+      <Text style={styles.bodyText}>4:19</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   body2: {
-    paddingTop: 170,
+    paddingBottom: 85,
     flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    justifyContent: "center",
+    padding: 20,
   },
 
   headerText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 50,
   },
 
   bodyText: {
-    color: '#bdbdbd',
+    color: "#bdbdbd",
     fontSize: 30,
   },
 
   img: {
     borderRadius: 20,
-    transform: [{scale: 1}],
-  }
+    transform: [{ scale: 1 }],
+  },
 });
